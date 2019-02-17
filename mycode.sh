1428bd4197d4a2d5f4bfc2546f21a40ea5d778e0
jarakcyc <dima_sudakov@mail.ru>
2019-02-17 12:17:26 -0800

    Hotfix haha

--- mycode.sh
+++ mycode.sh
@@ -13,16 +13,16 @@ for var in ${arr[*]}
 do
 	find ~ -depth -name "*.$var" -print0 | while IFS= read -r -d '' path
 	do
-	    base=$(basename --suffix=".$var"  "$path")
-	    new_path="$dir_name"/"$base.$var"
-	    echo "$new_path"
-	    counter=0
-	    while [ -f "$new_path" ]
-	    do
+		base=$(basename --suffix=".$var"  "$path")
+		new_path="$dir_name"/"$base.$var"
+		echo "$new_path"
+		counter=0
+		while [ -f "$new_path" ]
+		do
 	    	counter=$(($counter+1))
 	    	new_path="$dir_name"/"$base($counter).$var"
-	    done
-	    cp "$path" "$new_path"
+		done
+		cp "$path" "$new_path"
 	done
 done
 
