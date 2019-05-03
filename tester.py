ed4754bd2eefa3484b47553e22f86da256bbfa84
jarakcyc <dima_sudakov@mail.ru>
2019-05-03 03:53:28 -0700

    pytest

--- tester.py
+++ tester.py
@@ -1,24 +1,38 @@
 import ciphers
 import hack
+import pytest
 
-In = 'ATTACKATDAWN'
-assert(ciphers.encode_vigenere(In, 'LEMON') == 'LXFOPVEFRNHR')
+def test_1():
+    In = 'ATTACKATDAWN'
+    assert(ciphers.encode_vigenere(In, 'LEMON') == 'LXFOPVEFRNHR')
 
-In = 'ATTACkatdawn'
-assert(ciphers.encode_vigenere(In, 'LEMON') == 'LXFOPvefrnhr')
 
-In = 'LXFOPVEFRNHR'
-assert(ciphers.decode_vigenere(In, 'LEMON') == 'ATTACKATDAWN')
+def test_2():
+    In = 'ATTACkatdawn'
+    assert(ciphers.encode_vigenere(In, 'LEMON') == 'LXFOPvefrnhr')
 
-In = 'hello world'
-assert(ciphers.encode_caesar(In, 5) == 'mjqqt btwqi')
 
-In = 'mjqqt btwqi'
-assert(ciphers.decode_caesar(In, 5) == 'hello world')
+def test_3():
+    In = 'LXFOPVEFRNHR'
+    assert(ciphers.decode_vigenere(In, 'LEMON') == 'ATTACKATDAWN')
 
-In = 'ohoh upRvNkh Ul yhzopmyblzo'
-assert(ciphers.decode_caesar(In, 7) == 'haha niKoGda Ne rashifruesh')
 
+def test_4():
+    In = 'hello world'
+    assert(ciphers.encode_caesar(In, 5) == 'mjqqt btwqi')
+
+
+def test_5():
+    In = 'mjqqt btwqi'
+    assert(ciphers.decode_caesar(In, 5) == 'hello world')
+
+
+def test_6():
+    In = 'ohoh upRvNkh Ul yhzopmyblzo'
+    assert(ciphers.decode_caesar(In, 7) == 'haha niKoGda Ne rashifruesh')
+
+
+'''
 def test_hack(text, model):
     for k in range(26):
         i = open(text, 'r')
@@ -34,3 +48,4 @@ test_hack('text_1.txt', 'model.txt')
 # test_hack('text_2.txt', 'model.txt')
 
 print('ALL TESTS PASSED')
+'''
